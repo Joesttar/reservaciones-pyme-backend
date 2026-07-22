e29b0eba1ecd8244fe6e60923caee091f841b79e
Joe Garcia <joegarcia12_al@hotmail.com>
2026-07-22 17:15:34 -0600

    Connect Express to PostgreSQL database

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const pool = require('./config/db');
 
 app.get('/', (req, res) => {
     res.send('Servidor funcionando')
 })
 
+app.get('/test-db', async(req, res) => {
+    try {
+        const resultado = await pool.query('SELECT NOW()')
+        res.json(resultado.rows[0])
+    } catch (error) {
+        res.status(500).json({error: error.message, code:error.code})
+    }
+})
+
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`)
 })
+
+
 
